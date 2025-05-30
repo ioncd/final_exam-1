d887e2706ad32042c961a55f6647dccf0f618798
ioncd <mariondanganan11@gmail.com>
2025-05-30 08:21:07 +0800

    testing user and email

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,7 @@ Route::middleware('auth')->group(function () {
     Route::get('profile', [\App\Http\Controllers\ProfileController::class, 'show'])->name('profile.show');
     Route::put('profile', [\App\Http\Controllers\ProfileController::class, 'update'])->name('profile.update');
 
+    //Route::view('about', 'about')->name('about');
+    
+
 });
